fcc31d5067d888408adc494c5b44102d19b4ef0e
Abubakr Fazilov <abubakrfazilov98@gmail.com>
2020-02-06 20:18:48 +0200

    env vars

--- src/urls.js
+++ src/urls.js
@@ -1,10 +1,10 @@
 export default {
-    base: "http://localhost:3000/",
-    localebase: "https://angl-server.herokuapp.com/",
-    api:"api/v1.0/",
-    refreshPassword:"auth/refresh-password/",
-    remindPassword:"auth/token-refresh-password",
-    signIn:"auth/signin",
-    signUp:"auth/registration",
-    groups:"groups",
-}
+  base: process.env.BASE || "http://localhost:3000/",
+  localebase: "https://angl-server.herokuapp.com/",
+  api: "api/v1.0/",
+  refreshPassword: "auth/refresh-password/",
+  remindPassword: "auth/token-refresh-password",
+  signIn: "auth/signin",
+  signUp: "auth/registration",
+  groups: "groups"
+};
