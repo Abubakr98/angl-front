1e520af2c405987dc00ca363cbde6302fc4810e0
Abubakr Fazilov <abubakrfazilov98@gmail.com>
2020-02-08 13:46:19 +0200

    asd

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Home from "../pages/screens/Home"
-import {isAuthenticated, login} from './auth/auth'
-import {Redirect} from 'react-router-dom'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Home from "../pages/screens/Home";
+import { isAuthenticated, login } from "./auth/auth";
+import { Redirect } from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
   root: {
     backgroundColor: "#f7f7f7"
-  },
+  }
 }));
 
 export default function StickyFooter() {
@@ -15,7 +15,7 @@ export default function StickyFooter() {
 
   return (
     <div className={classes.root}>
-      {!isAuthenticated()?(<Redirect to="/" />):( <Home/>)}
+      {isAuthenticated() ? <Home /> : <Redirect to="/" />}
     </div>
   );
 }
