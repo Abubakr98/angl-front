ed4859fde05f46b736d34ac3ace3756a5044b43e
Abubakr Fazilov <abubakrfazilov98@gmail.com>
2020-04-23 22:24:13 +0300

    lazy load

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React from "react";
+import React, { lazy, Suspense  } from "react";
 import { connect } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
-
 
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Link from "@material-ui/core/Link";
-
+import { isAuthenticated } from "./pages/auth/auth";
+import { getUserData } from "./pages/auth/auth";
 import { Redirect } from "react-router-dom";
-import MenuBar from "./pages/components/MenuBar";
-import SidePanel from "./pages/components/SidePanel";
-import Alert from "./pages/components/Alert";
+
 import RemindPassword from "./pages/auth/RemindPassword";
 import RefreshPassword from "./pages/auth/RefreshPassword";
 import EmailVerify from "./pages/auth/EmailVerify";
-import { isAuthenticated } from "./pages/auth/auth";
-import Home from "./pages/main";
-import AdminWords from "./pages/adminPanel/words/words";
-import AdminGroups from "./pages/adminPanel/groups";
-import User from "./pages/user";
-import Tests from "./pages/Tests";
-import SignIn from "./pages/auth/SignIn";
-import SignUp from "./pages/auth/SignUp";
-import { getUserData } from "./pages/auth/auth";
+
+const MenuBar = lazy(() => import("./pages/components/MenuBar"));
+const SidePanel = lazy(() => import("./pages/components/SidePanel"));
+const Alert = lazy(() => import("./pages/components/Alert"));
+const AdminWords = lazy(() => import("./pages/adminPanel/words/words"));
+const AdminGroups = lazy(() => import("./pages/adminPanel/groups"));
+const User = lazy(() => import("./pages/user"));
+const Tests = lazy(() => import("./pages/Tests"));
+const SignIn = lazy(() => import("./pages/auth/SignIn"));
+const SignUp = lazy(() => import("./pages/auth/SignUp"));
+const Home = lazy(() => import("./pages/main"));
 
 function Copyright() {
   return (
@@ -38,16 +38,16 @@ function Copyright() {
     </Typography>
   );
 }
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
     flexDirection: "column",
     minHeight: "calc(100vh)",
-    paddingTop: "64px"
+    paddingTop: "64px",
   },
   main: {
     marginTop: theme.spacing(8),
-    marginBottom: theme.spacing(2)
+    marginBottom: theme.spacing(2),
   },
   footer: {
     padding: theme.spacing(3, 2),
@@ -55,8 +55,8 @@ const useStyles = makeStyles(theme => ({
     backgroundColor:
       theme.palette.type === "dark"
         ? theme.palette.grey[800]
-        : theme.palette.grey[200]
-  }
+        : theme.palette.grey[200],
+  },
 }));
 
 function App(props) {
@@ -64,9 +64,10 @@ function App(props) {
   props.setUserData(getUserData());
   return (
     <Router>
+       <Suspense fallback={<div>Loading...</div>}>
       <MenuBar />
       <div className={classes.root}>
-      <Alert />
+        <Alert />
         <SidePanel />
         <Switch>
           <Route path="/remind-password">
@@ -78,7 +79,7 @@ function App(props) {
           <Route path="/email-verify/:token">
             <EmailVerify />
           </Route>
-          <Route path="/sign-in" >
+          <Route path="/sign-in">
             <SignIn />
           </Route>
           <Route path="/sign-up">
@@ -109,23 +110,24 @@ function App(props) {
           </Container>
         </footer>
       </div>
+      </Suspense>
     </Router>
   );
 }
-const mapState = state => {
+const mapState = (state) => {
   return {
-    count: state.count
+    count: state.count,
   };
 };
 
 const mapDispatch = ({
   count: { addBy, addByAsync, remove, removeAsync },
-  userData: { setUserData }
+  userData: { setUserData },
 }) => ({
   addByOne: () => addBy(1),
   addByOneAsync: () => addByAsync(1),
   removeOne: () => remove(),
   removeOneAsync: () => removeAsync(),
-  setUserData: data => setUserData(data)
+  setUserData: (data) => setUserData(data),
 });
 export default connect(mapState, mapDispatch)(App);
